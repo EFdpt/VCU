acdc62fc171e0fe9246f8ef9acfe197dd2f697fa
jimbojambo63 <jimbo92@tiscali.it>
2017-08-20 14:05:47 +0200

    soglie freno ancora da mettere

--- FWCENTRALINAFSM/FWCENTRALINAFSM.ino
+++ FWCENTRALINAFSM/FWCENTRALINAFSM.ino
@@ -53,37 +53,27 @@ int plaus2 = 0;
 int plaus1 = 0;
 int BSPD = 1;
 
-/* valori dei pedali in milliVolt
-   analogReadResolution(12) ==> 4096=3300mV
 
-*/
-int th1Low = 1960;//2540
-int th1Up = 2583;//3150
-int th2Low = 2230;  //3100
-int th2Up = 2930; //3400
-int bkLow = 730;
-int bkUp = 980;
+int th1Low = 2920;//2540
+int th1Up = 4080;
+int th2Low = 3000;  //3100
+int th2Up = 4095; //3400
+int bkLow = 1090;
+int bkUp = 1180;
 int Upper = 1500;
 int Lower = 0;
 int SCthr=600;
+int dinamica1 = 885;  //75% di th1Up-th1Low utilizzato per mappa pedale non lineare
+int dinamica2 = 944; //80% della corsa del pedale
 
-
-int lunghezza = 64; //lunghezza array di acquisizione
-uint8_t lun = 6; //2^lun = lunghezza
+int lunghezza = 16; //lunghezza array di acquisizione
+uint8_t lun = 4; //2^lun = lunghezza
 int i=0;
 
-int RunTH = 2110; //25% della corsa del pedale
-int RunTH5 = 1958;
-int RunBK = 800; //10% della corsa del freno
-int RTDBK = 800; //pressione freno per RTD
-
-/*
- * inzializzazione array acquisizioni: 
- * prima di 64 acquisizioni la dinamica sarà molto smorzata a causa di tutti questi zeri
- */
-int ArrTh1[64], ArrTh2[64], ArrBk[64];
-int posArrTh1, posArrTh2, posArrBk;
-
+int RunTH = 3195; //25% della corsa del pedale
+int RunTH5 = 2959;
+int RunBK = 1160; //10% della corsa del freno
+int RTDBK = 1500; //pressione freno per RTD
 
 
 void setup() {
@@ -93,27 +83,19 @@ void setup() {
   pinMode(AIRGnd, OUTPUT);
   pinMode(PRE, OUTPUT);
   pinMode(BUZZ, OUTPUT);
-  //pinMode(AIRB, INPUT);
-  // pinMode(RTDB, INPUT);
+
   pinMode(BRAKEIN, INPUT);
   pinMode(AIRB, INPUT_PULLUP);
   pinMode(RTDB, INPUT_PULLUP);
   Serial.begin(9600);
   analogWriteResolution(12);
   analogReadResolution(12);
-  posArrTh1 = posArrTh2 = posArrBk = 0;
 
 
   //azzeramento TORQUE OUT and BRAKE OUT
   analogWrite(DAC0, 0); //brake
-  analogWrite(DAC1, map(th1Low+20, th1Low, th1Up, 0, 4095)); //torque
+  analogWrite(DAC1, map(th1Low, th1Low, th1Up, 0, 4095)); //torque
 
-//inizializzazione array acquisizioni
-  for(i=0; i< lunghezza; i++){
-    ArrTh1[i]=0;
-    ArrTh1[i]=0;
-    ArrBk[i]=0;
-    }
 }
 
 
@@ -172,25 +154,24 @@ Serial.print("bk ciclato: "); Serial.println(bk);
   if (analogRead(SC) > SCthr && bk > RTDBK && digitalRead(RTDB) == LOW) { //brake >800
 
     digitalWrite(BUZZ, HIGH);
-    delay(400);
+    delay(270);
     digitalWrite(BUZZ, LOW);
     delay(100);
     digitalWrite(BUZZ, HIGH);
-    delay(400);
+    delay(270);
     digitalWrite(BUZZ, LOW);
     delay(100);
     digitalWrite(BUZZ, HIGH);
-    delay(400);
+    delay(270);
     digitalWrite(BUZZ, LOW);
     delay(100);
     digitalWrite(BUZZ, HIGH);
-    delay(400);
+    delay(270);
     digitalWrite(BUZZ, LOW);
     delay(100);
     digitalWrite(BUZZ, HIGH);
-    delay(400);
+    delay(270);
     digitalWrite(BUZZ, LOW);
-    delay(100);
 
     //if(feedback RTD){
     RTD = 1;
@@ -217,37 +198,42 @@ void DRIVE() {
   */
   while (analogRead(SC) > SCthr && RTD && plaus1 && plaus2) {
 
-    acquisizioneTPS1();
-    acquisizioneTPS2();
-    acquisizioneBSE();
-    th1 = media(ArrTh1);
-    th2 = media(ArrTh2);
-    bk = media(ArrBk);
+    /*acquisizione diretta pedali*/
+    th1=analogRead(TPS1);
+    th2=analogRead(TPS2);
+    bk=analogRead(BRAKEIN);
 
-    //check plausibilità throttle
-    currMillis = millis();
-
-    if (currMillis - prevMillis >= plaus1Millis) {
-      prevMillis = currMillis;
-
-      /*
-        th1-th2 <= Upper bound && th1-th2 >= Lower bound
-       **/
-      if (th2 - th1 > Upper || th2 - th1 < Lower) plaus1 = 0;
-    }
-
-    /*
-      APPS >25% && brake attuato scatta seconda plausibilità
-    */
-    if (th1 > RunTH && bk > RunBK) plaus2 = 0;
+//    //check plausibilità throttle
+//    currMillis = millis();
+//
+//    if (currMillis - prevMillis >= plaus1Millis) {
+//      prevMillis = currMillis;
+//
+//      /*
+//        th1-th2 <= Upper bound && th1-th2 >= Lower bound
+//       **/
+//      if (th2 - th1 > Upper || th2 - th1 < Lower) plaus1 = 0;
+//    }
+//
+//    /*
+//      APPS >25% && brake attuato scatta seconda plausibilità
+//    */
+//    if (th1 > RunTH && bk > RunBK) plaus2 = 0;
 
     /*drive!!*/
     if (plaus1 && plaus2) {
-
-      analogWrite(DAC1, map(th1, th1Low, th1Up, 0, 4095));
+      if(th1<th1Low)  th1=th1Low;
+      if(th1>th1Up) th1=th1Up-20;
+  
+//      //mappa spezzata su richiesta coppia
+//      if (th1 <= dinamica1 + th1Low)
+//        analogWrite(DAC1, map(th1, th1Low, dinamica2 + th1Low, 0, 2047));
+//      else
+//        analogWrite(DAC1, map(th1, dinamica2 + th1Low, th1Up, 2048, 4095));
+        analogWrite(DAC1, map(th1, th1Low, th1Up, 0, 4095));
       analogWrite(DAC0, 0);
 
-      Serial.print("TH1 media: "); Serial.print(th1); Serial.print("   BREAK media: "); Serial.println(bk);
+      Serial.print("TH1: "); Serial.print(th1); Serial.print("   BREAK: "); Serial.println(bk);
     }
 
   }// end while
@@ -257,7 +243,7 @@ void DRIVE() {
 
   /*scatto plausibilità TPS o scollegamento BRAKE*/
   if (!plaus1 || bk < 500) {
-    analogWrite(DAC1, 0);
+    analogWrite(DAC1, map(th1Low, th1Low, th1Up, 0, 4095)); //TORQUE
     analogWrite(DAC0, 0);
     current_state = 3;
   }
@@ -268,9 +254,10 @@ void DRIVE() {
 
   */
   if (!plaus2) {
-    while (media(ArrTh1) > RunTH5) {
-      acquisizioneTPS1();
-      acquisizioneBSE();
+    while (analogRead(TPS1) > RunTH5) {
+      th1=analogRead(TPS1);
+      th2=analogRead(TPS2);
+      bk=analogRead(BRAKEIN);
       Serial.println("   FRENO + ACCELERATORE !!!");
       //      //BYPASS BSPD
       //      if (media(bk) > 870) {
@@ -295,13 +282,9 @@ void DRIVE() {
  */
 void NOTDRIVE() {
   if (analogRead(SC) < SCthr) current_state = 0;
-  acquisizioneTPS1();
-  acquisizioneTPS2();
-  acquisizioneBSE();
-
-  th1 = media(ArrTh1);
-  th2 = media(ArrTh2);
-  bk = media(ArrBk);
+  th1=analogRead(TPS1);
+  th2=analogRead(TPS2);
+  bk=analogRead(BRAKEIN);
   /*
     th1-th2 <= Upper bound && th1-th2 >= Lower bound && bk < 10%
    **/
@@ -320,8 +303,7 @@ void(*state_table[])(void) = {STAND, HVON, DRIVE, NOTDRIVE};
 
 void loop() {
   state_table[current_state]();
-  //delay(5);
-  currMillis2 = millis();
+    currMillis2 = millis();
 
   if (currMillis2 - prevMillis2 >= plaus1Millis2) {
     prevMillis2 = currMillis2;
@@ -330,21 +312,5 @@ void loop() {
     Serial.print("    TPS2: "); Serial.print(analogRead(TPS2)); Serial.print("    Brake IN: "); Serial.println(analogRead(BRAKEIN));
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
