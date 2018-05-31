95ca4034d27dcc34be43659b6d4d56775e2e06a3
ArellaMatteo <arella.1646983@studenti.uniroma1.it>
2018-05-31 20:54:50 +0200

    Test motor control over CAN bus

--- VCU.ino
+++ VCU.ino
@@ -1,14 +1,30 @@
 #include <due_can.h>
 #include <DueTimer.h>
 
+/**
+  PINOUT ENCODER MOTORE
+
+  MOTORE          -> INVERTER
+  ROSSO   VCC     -> 28
+  BLU     GND     -> 24
+  BIANCO  CLK+    -> 9
+  VERDE   DATA+   -> 11
+  MARR    CLK-    -> 10
+  GIALLO  DATA-   -> 12
+*/
+
 #define INVERTER_NODE_ID      (0x01)
 #define VCU_NODE_ID           (0x11)
 #define SYNC_PERIOD           (5000) // 5ms
 
 #define CAN_FUNZ              Can0
 
+#define INVERTER_TORQUE_MAX   (32767)
+
 volatile bool       inverter_online     = false;
 volatile bool       inverter_configured = false;
+
+volatile uint16_t   torque_actual_val   = 0;
 
 uint32_t vendorID;
 
@@ -27,6 +43,10 @@ void CAN_GENERAL_CB(CAN_FRAME* frame) {
   if (frame -> id == (0x700 + INVERTER_NODE_ID)) {
 
     inverter_online = true;
+
+    if (inverter_online)
+      return;
+
     Serial.println("Received boot-up message from inverter");
 
     //Check inverter vendor ID (SDO client request)
@@ -41,6 +61,9 @@ void CAN_GENERAL_CB(CAN_FRAME* frame) {
     Serial.println("VendorID SDO client request sent");
 
   } else if (frame -> id == 0x580 + INVERTER_NODE_ID) {
+
+    if (inverter_configured)
+        return;
 
     vendorID = frame -> data.low;
     Serial.print("SDO server response with vendorID: ");
@@ -81,10 +104,11 @@ void CAN_GENERAL_CB(CAN_FRAME* frame) {
     CAN_FUNZ.sendFrame(output);
 
     inverter_configured = true;
-    Serial.println("Inverter successfully configured");
 
     // send periodic SYNC
     DueTimer::getAvailable().attachInterrupt(send_sync).start(SYNC_PERIOD);
+  } else if (frame -> id == 0x180 + INVERTER_NODE_ID) { // inverter torque actual val
+    torque_actual_val = frame -> data.s3;
   }
 }
 
@@ -94,7 +118,7 @@ void torque_request(uint16_t torque) {
   output.id = 0x00000200 + (uint8_t) VCU_NODE_ID;
   output.length = 8;
   output.data.high = 0x0F000000;
-  output.data.low = 0x00000000 | torque;
+  output.data.low = 0x00000000 | map(torque, 0, 4095, 0, INVERTER_TORQUE_MAX);
   output.extended = 0;
   CAN_FUNZ.sendFrame(output);
 }
@@ -120,7 +144,6 @@ void CAN_GENERAL_CONSUMER_CB(CAN_FRAME* frame) {
 void CanBegin(){
 
   Serial.begin(9600);
-
   while (!Serial);
 
   Serial.println("Serial started");
@@ -128,15 +151,7 @@ void CanBegin(){
   while (!CAN_FUNZ.begin(CAN_BPS_500K)){
       Serial.println("CAN baudrate initialization ERROR");
   }
-
-  // boot-up message
-  //CAN_FUNZ.setRXFilter(0, 0x00000700 + INVERTER_NODE_ID, 0x1FFFFFFF, false);
-  //CAN_FUNZ.setCallback(0, INVERTER_BOOT_UP_CB);
-
-  // inverter vendor ID
-  //CAN_FUNZ.setRXFilter(1, 0x00000580 + INVERTER_NODE_ID, 0x1FFFFFFF, false);
-  //CAN_FUNZ.setCallback(1, INVERTER_VENDOR_ID_CB);
-
+  
   uint32_t i = 0;
 
   for (; i < 8; i++) {
@@ -147,11 +162,21 @@ void CanBegin(){
   Serial.println("CAN mailboxes configured");
 
   while (!inverter_online || !inverter_configured) {}
+
+  Serial.println("Inverter successfully configured");
 }
 
 void setup() {
   CanBegin();
+  analogReadResolution(12);
+  torque_request(0);
 }
 
+volatile uint16_t apps_value;
+
 void loop() {
+    apps_value = (apps_value + analogRead(A0)) / 2;
+    torque_request(apps_value);
+    Serial.print("Torque actual value = ");
+    Serial.println(torque_actual_val);
 }
